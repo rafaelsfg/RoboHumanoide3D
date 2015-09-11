b81d0547466f011315f964d896be1f288673ca5a
Rafael Andrade <rafaelsandrade@gmail.com>
2015-09-11 16:07:35 -0300

    Update Robo_humanoide.cpp

--- Robo_humanoide.cpp
+++ Robo_humanoide.cpp
@@ -1,7 +1,7 @@
 /*
  * Robo_Humanoide.cpp
  *
- * Copyright 2014 Rafael Andrade
+ * Copyright 2014 Rafael Andrade <rafaelsandrade@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
